f80e670fff7c4e4d99cbef08947f6fbc15f705b9
Michael-Ngochi <ngoochimike@gmail.com>
2025-06-09 13:14:11 +0300

    added route methods

--- server/app.py
+++ server/app.py
@@ -4,5 +4,37 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_parameter(parameter):
+    print(parameter)
+    return(parameter)
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    result = '\n'.join(str(i) for i in range(parameter))+'\n'
+    return result
+
+@app.route('/math/<int:num1>/<operator>/<int:num2>')
+def math(num1,num2,operator):
+    answer=0
+    if operator == 'div':
+       answer=num1/num2
+    elif operator == '+':
+        answer=num1+num2
+    elif operator == '-':
+         answer=num1-num2
+    elif operator == '*':
+        answer=num1*num2
+    elif operator == '%':
+        answer=num1%num2
+    else:
+        return 'Invalid operator'
+    return f"{answer}"
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
